e4b605942f90a1e01949018cedd67faf203c164e
Sofiane Hamlaoui <shamlaou@z1r8p1.le-101.fr>
2019-08-20 23:42:58 +0200

    C07_ex03_main_alt

--- C07_mains/main_03.c
+++ C07_mains/main_03.c
@@ -6,7 +6,7 @@
 /*   By: ahcherch <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/08/15 18:26:47 by ahcherch     #+#   ##    ##    #+#       */
-/*   Updated: 2019/08/19 16:02:02 by ahcherch    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/08/20 23:41:11 by shamlaou    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,10 +15,9 @@ char *ft_strjoin(int size, char **strs, char *sep);
 
 #include <stdio.h>
 
-int		main()
+int		main(int ac, char **av)
 {
-	char *strs[] = {"abcdef", "ghijk" ,"lmnopq"};
 	char sep[] = "*";
-	printf("%s", ft_strjoin(3, strs, sep));
+	printf("%s", ft_strjoin(ac, av, sep));
 
 }
